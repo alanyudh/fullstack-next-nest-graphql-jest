8de8d49450d74a1d3be8e44e40c9092aa442162e
Alan <alan@c-suite.sg>
2021-05-11 18:37:37 +0700

    revise default gql endpoint

--- packages/web/lib/apollo-client.ts
+++ packages/web/lib/apollo-client.ts
@@ -1,9 +1,14 @@
 import fetch from 'cross-fetch'
 import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client'
 
+const uri =
+  !process.env.NEXT_PUBLIC_GQL_HOST || process.env.NEXT_PUBLIC_GQL_HOST === ''
+    ? 'http://localhost:4000/graphql'
+    : process.env.NEXT_PUBLIC_GQL_HOST
+
 const client = new ApolloClient({
-  link: new HttpLink({ uri: process.env.NEXT_PUBLIC_GQL_HOST, fetch }),
-  uri: process.env.NEXT_PUBLIC_GQL_HOST,
+  link: new HttpLink({ uri, fetch }),
+  uri,
   cache: new InMemoryCache(),
 })
 
